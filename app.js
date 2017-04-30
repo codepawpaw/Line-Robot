e69dced570e44c8436dcc49e3d195a1f6f81f479
Jonathan <jonathannael12@gmail.com>
2017-04-30 14:33:35 +0000

    update worker

--- app.js
+++ app.js
@@ -20,30 +20,8 @@ var cluster = require('cluster');
 var os = require('os');
 
 if (cluster.isMaster) {
-  // Fork workers
-  // Server
-  if(os.cpus().length > 4) {
-    // Big server
-    if(os.cpus().length >= 16) {
-      for (var i = 0; i < os.cpus().length; i++) {
-        cluster.fork();
-      }
-    }
-    // Small server
-    else {
-     for (var i = 0; i < 8; i++) {
-        cluster.fork();
-      }
-    }
-    for (var i = 0; i < 8; i++) {
+  for (var i = 0; i < 1; i++) {
       cluster.fork();
-    }
-  }
-  // Desktop
-  else {
-    for (var i = 0; i < 2; i++) {
-      cluster.fork();
-    }
   }
 
   Object.keys(cluster.workers).forEach(function(id) {
