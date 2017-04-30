62f1a1e80ee9ea707cfdca3466ac2f636eb26fe7
Jonathan <jonathannael12@gmail.com>
2017-04-30 14:59:43 +0000

    update mysql location

--- models/index.js
+++ models/index.js
@@ -3,10 +3,10 @@ var Sequelize = require('sequelize');
 
 // initialize database connection
 var sequelize = new Sequelize(
-  'event_bot',
-  'quiz',
-  'wv55ZWWl-production',
-  {"dialect": "mysql", "host": 'localhost', "port": '3306',
+  'sql12171993',
+  'sql12171993',
+  'cydwSk74pT',
+  {"dialect": "mysql", "host": 'sql12.freemysqlhosting.net', "port": '3306',
     "logging": false, "define": {underscored: true}
   }
 );
